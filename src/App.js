5907d50f62b3e0c9e3762cb89595bc0193e84ac4
kam <kam.hungerford@gmail.com>
2022-05-23 15:33:20 -0500

    Fixed issues with routes displaying a blank page with newer syntax

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Dashboard from './components/dashboard'
 import Preferences from './components/preferences'
 
@@ -8,8 +8,10 @@ export default function App() {
       <div className="App">
         <h1>Friend Space</h1>
         <Router>
-            <Route exact path="/dashboard" component={Dashboard} />
-            <Route exact path="/preferences" component={Preferences} />
+          <Routes>
+            <Route exact path="/dashboard" element={<Dashboard />} />
+            <Route exact path="/preferences" element={<Preferences />} />
+          </Routes>
         </Router>
       </div>
   )
