90590b0a76105d32a07c3f0abcf898526a24e646
kam <kam.hungerford@gmail.com>
2022-05-23 15:38:30 -0500

    Created login form on Login component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Dashboard from './components/dashboard'
-import Preferences from './components/preferences'
+import Preferences from './components/Preferences'
 
 export default function App() {
   return (
