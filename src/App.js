1eb03be0e21d434e0dabde9ff94c83a12458e196
kam <kam.hungerford@gmail.com>
2022-05-23 15:45:24 -0500

    Added some css to login table and added conditional statement to App to show/hide login form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import React, { useState } from 'react'
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Dashboard from './components/dashboard'
+import Login from './components/Login/Login'
 import Preferences from './components/Preferences'
 
 export default function App() {
+  const [token, setToken] = useState()
+
+  if(!token) {
+    return <Login setToken={setToken} />
+  }
   return (
       <div className="wrapper">
         <h1>Friend Space</h1>
